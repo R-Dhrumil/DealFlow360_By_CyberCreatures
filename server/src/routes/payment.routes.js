0669bc6ca9e2b93b1bf61be45ab1e3bbad6645a0
R-Dhrumil <dhrumilrana516@gmail.com>
2026-09-06 03:57:43 +0530

    feat: add authentication middleware to payment routes

--- server/src/routes/payment.routes.js
+++ server/src/routes/payment.routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/payment.controller');
 const asyncWrap = require('../utils/asyncWrap');
+const { auth } = require('../middleware/auth');
 
-router.get('/', asyncWrap((req, res) => paymentController.getCompanyPayments(req, res)));
+router.get('/', auth, asyncWrap((req, res) => paymentController.getCompanyPayments(req, res)));
 
 module.exports = router;
